cfc3a50619c9a2a79dfb1332c05bfadb953cccdc
Goutham949n <Goutham949n@gmail.com>
2018-10-23 23:12:06 -0400

    including crossOrigins

--- src/main/java/com/web/awstest/awstest/BmiController.java
+++ src/main/java/com/web/awstest/awstest/BmiController.java
@@ -1,12 +1,14 @@
 package com.web.awstest.awstest;
 
 import org.springframework.util.NumberUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:4200"},maxAge = 4800, allowCredentials = "false")
 @RestController
 @RequestMapping(path = "/bmi")
 public class BmiController {
